57d1db60f20fdceea3b316215f6bc7253807d47f
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-02-27 21:26:55 -0800

    deleted unnecessary files

--- data_processing.py
+++ data_processing.py
@@ -1,24 +1,14 @@
 import pandas as pd
-from bs4 import BeautifulSoup
+import data_querying
 
-def process_html_str(raw_html):
+
+def load_data(filename):
     """
-    Use BeautifulSoup to process raw 
-    html and extract text.
-    
-    Args: 
-    (str) raw_html: raw string of HTML ripe for BS4 parsing
-    Returns:
-    (str) text: raw text extracted from HTML
+    Load data from file into dict format.
+    For compatibility with existing DetectGPT code.
+    Expects that the df loaded in has 'original, sampled'
+    columns.
     """
-    soup = BeautifulSoup(raw_html)
-    return soup.get_text(' ', True)
-
-def main(filepath):
-    df = pd.read_json(filepath, orient='records', encoding_errors='replace')
-    df['question'] = df['question'].apply(process_html_str)
-    df['answer'] = df['answer'].apply(process_html_str)
-    df.to_csv('preprocessing_text.json')
-
-if __name__=='__main__':
-    main('data/preprocessing_html_data.json')
+    df = pd.read_csv(filename)
+    return {'original': df['original'].values.tolist(),
+            'sampled': df['sampled'].values.tolist()}
--- scripts/ChatGPT.sh
+++ scripts/ChatGPT.sh
@@ -1,4 +1,4 @@
 ##! /bin/bash
 
 python data_querying.py armaanrashid@rocketmail.com JTJ6AAWytZdtZBv xsum -f XSum/xsum_human.csv \
-        -m "Complete the following text: \n\n" -r -p -v -d XSum/xsum_chatgpt.csv
+        -m "Complete the following text: " -r -p -v -d XSum/xsum_chatgpt.csv
