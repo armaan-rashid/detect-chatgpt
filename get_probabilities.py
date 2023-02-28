c9cb8ee24d893169ea2d37224f0c105e4f259ed0
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-02-27 22:00:23 -0800

    added new probability file

---
+++ get_probabilities.py
@@ -0,0 +1,17 @@
+import openai
+
+def get_ll(text):
+    if args.openai_model:        
+        kwargs = { "engine": args.openai_model, "temperature": 0, "max_tokens": 0, "echo": True, "logprobs": 0}
+        r = openai.Completion.create(prompt=f"<|endoftext|>{text}", **kwargs)
+        result = r['choices'][0]
+        tokens, logprobs = result["logprobs"]["tokens"][1:], result["logprobs"]["token_logprobs"][1:]
+
+        assert len(tokens) == len(logprobs), f"Expected {len(tokens)} logprobs, got {len(logprobs)}"
+
+        return np.mean(logprobs)
+    else:
+        with torch.no_grad():
+            tokenized = base_tokenizer(text, return_tensors="pt").to(DEVICE)
+            labels = tokenized.input_ids
+            return -base_model(**tokenized, labels=labels).loss.item()
