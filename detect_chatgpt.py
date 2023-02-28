f8f5e7e813ca6da9b16dbcdf559ac62e67f134ff
Julia Park <juliapark@DN0a22655a.SUNet>
2023-02-27 21:24:02 -0800

    Added masking model, started perturb function

--- detect_chatgpt.py
+++ detect_chatgpt.py
@@ -2,38 +2,98 @@
 Project model.
 
 TASKS: 
-1. load mask model correctly, correct+finish load_mask_model function
+[DONE] 1. load mask model correctly, correct+finish load_mask_model function
 2. correct+finish data generating functions (line 87), pass data into get_perturbation_results
 3. correct+finish get_perturbation_results
 4. correct+finish main code
 """
 
+import transformers
+import functools
 import detect_gpt
 
 DEVICE = 'cuda' if cuda.is_available() else 'cpu'
 
-# TODO: Load and return mask_model
 def load_mask_model():
-    # START unchecked line
-    mask_model = transformers.AutoModelForSeq2SeqLM.from_pretrained(mask_filling_model_name, **int8_kwargs, **half_kwargs, cache_dir=cache_dir)
-    # END unchecked line
-
-    # START unchecked function
+    mask_model = transformers.AutoModelForSeq2SeqLM.from_pretrained("t5-3b") # can be cached. 
+  
     print('MOVING MASK MODEL TO GPU...', end='', flush=True)
-    
     start = time.time()
-
-    if args.openai_model is None:
-        base_model.cpu()
-    if not args.random_fills:
-        mask_model.to(DEVICE)
+    mask_model.to(DEVICE)
     print(f'DONE ({time.time() - start:.2f}s)')
-    # END unchecked function
 
     return mask_model
 
+
+
+
+# helper function for perturb_texts
+# TODO unchecked function
+def perturb_texts_(texts, span_length, pct, ceil_pct=False):
+    # START unchecked function
+    if not args.random_fills:
+        masked_texts = [tokenize_and_mask(x, span_length, pct, ceil_pct) for x in texts]
+        raw_fills = replace_masks(masked_texts)
+        extracted_fills = extract_fills(raw_fills)
+        perturbed_texts = apply_extracted_fills(masked_texts, extracted_fills)
+
+        # Handle the fact that sometimes the model doesn't generate the right number of fills and we have to try again
+        attempts = 1
+        while '' in perturbed_texts:
+            idxs = [idx for idx, x in enumerate(perturbed_texts) if x == '']
+            print(f'WARNING: {len(idxs)} texts have no fills. Trying again [attempt {attempts}].')
+            masked_texts = [tokenize_and_mask(x, span_length, pct, ceil_pct) for idx, x in enumerate(texts) if idx in idxs]
+            raw_fills = replace_masks(masked_texts)
+            extracted_fills = extract_fills(raw_fills)
+            new_perturbed_texts = apply_extracted_fills(masked_texts, extracted_fills)
+            for idx, x in zip(idxs, new_perturbed_texts):
+                perturbed_texts[idx] = x
+            attempts += 1
+    else:
+        if args.random_fills_tokens:
+            # tokenize base_tokenizer
+            tokens = base_tokenizer(texts, return_tensors="pt", padding=True).to(DEVICE)
+            valid_tokens = tokens.input_ids != base_tokenizer.pad_token_id
+            replace_pct = args.pct_words_masked * (args.span_length / (args.span_length + 2 * args.buffer_size))
+
+            # replace replace_pct of input_ids with random tokens
+            random_mask = torch.rand(tokens.input_ids.shape, device=DEVICE) < replace_pct
+            random_mask &= valid_tokens
+            random_tokens = torch.randint(0, base_tokenizer.vocab_size, (random_mask.sum(),), device=DEVICE)
+            # while any of the random tokens are special tokens, replace them with random non-special tokens
+            while any(base_tokenizer.decode(x) in base_tokenizer.all_special_tokens for x in random_tokens):
+                random_tokens = torch.randint(0, base_tokenizer.vocab_size, (random_mask.sum(),), device=DEVICE)
+            tokens.input_ids[random_mask] = random_tokens
+            perturbed_texts = base_tokenizer.batch_decode(tokens.input_ids, skip_special_tokens=True)
+        else:
+            masked_texts = [tokenize_and_mask(x, span_length, pct, ceil_pct) for x in texts]
+            perturbed_texts = masked_texts
+            # replace each <extra_id_*> with args.span_length random words from FILL_DICTIONARY
+            for idx, text in enumerate(perturbed_texts):
+                filled_text = text
+                for fill_idx in range(count_masks([text])[0]):
+                    fill = random.sample(FILL_DICTIONARY, span_length)
+                    filled_text = filled_text.replace(f"<extra_id_{fill_idx}>", " ".join(fill))
+                assert count_masks([filled_text])[0] == 0, "Failed to replace all masks"
+                perturbed_texts[idx] = filled_text
+    # END unchecked function
+    return perturbed_texts
+
+# called by get_perturbation_results
+# texts: array of texts to perturb
+# span_length: to pass into helper perturb_texts_
+# pct: to pass into helper perturb_texts_
+# ceil_pct: to pass into helper perturb_texts_
+# chunk_size: the number of texts to pass into helper perturb_texts_ each call
+def perturb_texts(texts, span_length, pct, ceil_pct=False, chunk_size=20):
+    outputs = []
+    for i in tqdm.tqdm(range(0, len(texts), chunk_size), desc="Applying perturbations"):
+        outputs.extend(perturb_texts_(texts[i:i + chunk_size], span_length, pct, ceil_pct=ceil_pct))
+    return outputs
+
+
 # TODO: return likelihoods from perturbed samples
-def get_perturbation_results(data, span_length=10, n_perturbations=1, n_samples=500):
+def get_perturbation_results(data, pct_words_masked=0.3, span_length=10, n_perturbations=1, n_samples=500):
     mask_model = load_mask_model()
 
     torch.manual_seed(0)
@@ -43,9 +103,9 @@ def get_perturbation_results(data, span_length=10, n_perturbations=1, n_samples=
     original_text = data["original"]
     sampled_text = data["sampled"]
 
-    # START unchecked function
-    perturb_fn = functools.partial(perturb_texts, span_length=span_length, pct=args.pct_words_masked)
+    perturb_fn = functools.partial(perturb_texts, span_length=span_length, pct=pct_words_masked)
 
+    # START unchecked function
     p_sampled_text = perturb_fn([x for x in sampled_text for _ in range(n_perturbations)])
     p_original_text = perturb_fn([x for x in original_text for _ in range(n_perturbations)])
     for _ in range(n_perturbation_rounds - 1):
