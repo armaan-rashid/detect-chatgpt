6f49ff61cfaf9a9939105265ad1a05f5b968680b
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-03-10 10:59:28 -0800

    processed data

--- scripts/data_processing.sh
+++ scripts/data_processing.sh
@@ -1,8 +1,14 @@
 ##! /bin/bash
 
-python data_processing.py merge --orig_file SQuAD/squad_raw.csv --sampled_file SQuAD/squad_responses_00.csv \
-    --orig_cols "answers" --sampled_cols "responses" --outfile SQuAD/squad_00.csv
-python data_processing.py merge --orig_file SQuAD/squad_raw.csv --sampled_file SQuAD/squad_responses_50.csv \
-    --orig_cols "answers" --sampled_cols "responses" --outfile SQuAD/squad_50.csv
-python data_processing.py merge --orig_file SQuAD/squad_raw.csv --sampled_file SQuAD/squad_responses_100.csv \
-    --orig_cols "answers" --sampled_cols "responses" --outfile SQuAD/squad_100.csv
+python data_processing.py strip --strip_file XSum/xsum_responses_150.csv --strip_col prompts --strip_msg "Complete the following text: "
+python data_processing.py strip --strip_file XSum/xsum_responses_200.csv --strip_col prompts --strip_msg "Complete the following text: "
+
+python data_processing.py merge --orig_file XSum/xsum_raw.csv --sampled_file XSum/xsum_responses_150.csv \
+    --orig_cols "articles" --sampled_cols "prompts, responses" --outfile XSum/xsum_150.csv
+python data_processing.py merge --orig_file XSum/xsum_raw.csv --sampled_file XSum/xsum_responses_200.csv \
+    --orig_cols "articles" --sampled_cols "prompts, responses" --outfile XSum/xsum_200.csv
+
+python data_processing.py merge --orig_file SQuAD/squad_raw.csv --sampled_file SQuAD/squad_responses_150.csv \
+    --orig_cols "answers" --sampled_cols "responses" --outfile SQuAD/squad_150.csv
+python data_processing.py merge --orig_file SQuAD/squad_raw.csv --sampled_file SQuAD/squad_responses_200.csv \
+    --orig_cols "answers" --sampled_cols "responses" --outfile SQuAD/squad_200.csv
