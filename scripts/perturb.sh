88387d6f0fe8493407babf191af5379dc2732bea
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-03-17 16:37:33 -0700

    maybe the lord smiles

--- scripts/perturb.sh
+++ scripts/perturb.sh
@@ -1,8 +1,6 @@
 ##! /bin/bash
 
-python perturb.py XSum/xsum_50.csv -c 5 -k 500 -n 100 -s 2 -p 0.15 -w Perturbations/xsum_t50_k500_n100_s2_p15.csv
-python perturb.py XSum/xsum_100.csv -c 5 -k 500 -n 100 -s 2 -p 0.15 -w Perturbations/xsum_t100_k500_n100_s2_p15.csv
-
-python perturb.py WritingPrompts/wp_00.csv -c 5 -k 500 -n 100 -s 2 -p 0.15 -w Perturbations/wp_t00_k500_n100_s2_p15.csv
-python perturb.py WritingPrompts/wp_50.csv -c 5 -k 500 -n 100 -s 2 -p 0.15 -w Perturbations/wp_t50_k500_n100_s2_p15.csv
-python perturb.py WritingPrompts/wp_100.csv -c 5 -k 500 -n 100 -s 2 -p 0.15 -w Perturbations/wp_t100_k500_n100_s2_p15.csv
+python perturb.py XSum/xsum_50.csv -c 50 -o Perturbations/xsum_t00_k500_n100_s2_p15.csv
+python perturb.py XSum/xsum_100.csv -c 50 -o Perturbations/xsum_t00_k500_n100_s2_p15.csv
+python perturb.py WritingPrompts/wp_50.csv -c 50 -o Perturbations/xsum_t00_k500_n100_s2_p15.csv
+python perturb.py WritingPrompts/wp_100.csv -c 50 -o Perturbations/xsum_t00_k500_n100_s2_p15.csv
