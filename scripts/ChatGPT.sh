30c570a49c317ca09453f1c4a48e18cac3bdab0b
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-03-17 15:57:19 -0700

    writingprompts run

--- scripts/ChatGPT.sh
+++ scripts/ChatGPT.sh
@@ -1,3 +1,7 @@
 ##! /bin/bash
-python detect_chatgpt.py xsum --perturbation_file Perturbations/xsum_t00_k500_n100_s2_p15.csv --huggingface_query_models EleutherAI/gpt-neo-2.7B -n 100 -k 500 -d xsum_neo
-python detect_chatgpt.py xsum --perturbation_file Perturbations/xsum_t00_k500_n100_s2_p15.csv --openai_query_models babbage -n 100 -k 500 -d xsum_babbage
+
+python detect_chatgpt.py wp --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --huggingface_query_models gpt2 -n 100 -k 500 -d wp_gpt2
+python detect_chatgpt.py wp --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --huggingface_query_models EleutherAI/gpt-j-6B -n 100 -k 500 -d wp_gptj
+python detect_chatgpt.py wp --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --huggingface_query_models facebook/opt-2.7b -n 100 -k 500 -d wp_opt
+python detect_chatgpt.py wp --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --huggingface_query_models EleutherAI/gpt-neo-2.7B -n 100 -k 500 -d wp_neo
+python detect_chatgpt.py wp --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --openai_query_models babbage -n 100 -k 500 -d wp_babbage
