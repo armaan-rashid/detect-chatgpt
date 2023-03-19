6023d3a2c380f22df9cbc0f08bd244514a693d52
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-03-18 21:45:29 -0700

    typos

--- scripts/detect_wp_t00.sh
+++ scripts/detect_wp_t00.sh
@@ -1,6 +1,6 @@
 ##! /bin/bash
 
-python detect_chatgpt.py wp 00 --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --huggingface_query_models gpt2
-python detect_chatgpt.py wp 00 --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --huggingface_query_models facebook/opt-2.7b 
-python detect_chatgpt.py wp 00 --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --huggingface_query_models EleutherAI/gpt-neo-2.7B 
-python detect_chatgpt.py wp 00 --perturbation_file Perturbations/wp_t00_k500_n100_s2_p15.csv --openai_query_models babbage 
+python detect_chatgpt.py wp 00 --perturbation_file Perturbations/WritingPrompts_t00_k500_n100_s2_p15.csv --huggingface_query_models gpt2
+python detect_chatgpt.py wp 00 --perturbation_file Perturbations/WritingPrompts_t00_k500_n100_s2_p15.csv --huggingface_query_models facebook/opt-2.7b 
+python detect_chatgpt.py wp 00 --perturbation_file Perturbations/WritingPrompts_t00_k500_n100_s2_p15.csv --huggingface_query_models EleutherAI/gpt-neo-2.7B 
+python detect_chatgpt.py wp 00 --perturbation_file Perturbations/WritingPrompts_t00_k500_n100_s2_p15.csv --openai_query_models babbage 
