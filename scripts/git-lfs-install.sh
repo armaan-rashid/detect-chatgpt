514059781edbd9e085e87842b8c4d1960eaffa57
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-03-19 02:38:52 -0700

    huh

--- scripts/git-lfs-install.sh
+++ scripts/git-lfs-install.sh
@@ -1,6 +1,5 @@
 ##! /bin/bash
 
-sudo apt update && sudo apt upgrade
-sudo apt install curl
+sudo apt update -y && sudo apt upgrade -y
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs=3.3.0
