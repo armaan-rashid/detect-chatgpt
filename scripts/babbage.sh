4256c1b847e2258d47b37d4c05aa5c585a13d128
armaan-rashid <119842350+armaan-rashid@users.noreply.github.com>
2023-03-19 21:43:17 -0700

    huh

--- scripts/babbage.sh
+++ scripts/babbage.sh
@@ -1,4 +1,5 @@
 python detect_chatgpt.py wp 00 --perturbation_file Perturbations/WritingPrompts_t00_k500_n100_s2_p15.csv --openai_query_models babbage -e
+python detect_chatgpt.py wp 00 --perturbation_file Perturbations/WritingPrompts_t50_k500_n100_s2_p15.csv --openai_query_models babbage -e
 python detect_chatgpt.py wp 100 --perturbation_file Perturbations/WritingPrompts_t100_k499_n100_s2_p15.csv --openai_query_models babbage -e
 python detect_chatgpt.py xsum 50 --perturbation_file Perturbations/XSum_t50_k500_n100_s2_p15.csv --openai_query_models babbage -e
 python detect_chatgpt.py xsum 100 --perturbation_file Perturbations/XSum_t100_k500_n100_s2_p15.csv --openai_query_models babbage -e
